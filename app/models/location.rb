507a67f7d4b2fd37db74e5ff32d444f713af4660
Jonathon <jonathon.nordquist@gmail.com>
2016-04-09 11:05:50 -0700

    Add custom error messages, html tags to new location form

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,9 +4,9 @@ class Location < ActiveRecord::Base
 	LATTITUDE_FORMAT = /\d{1,2}\.\d{4}/
 	LONGITUDE_FORMAT = /\d{1,3}\.\d{4}/
 
-	validates :lattitude, presence: true, format: { with: /\d{1,2}\.\d{4}/ }
-	validates :hemi_n_s, presence: true
-	validates :longitude, presence: true, format: { with: /\d{1,3}\.\d{4}/ }
-	validates :hemi_e_w, presence: true
 	validates :client_id, presence: true
+	validates :lattitude, presence: true, format: { with: /\d{1,2}\.\d{4}/ }
+	validates :hemi_n_s, presence: { message: "N\\S Hemisphere must be specified."}
+	validates :longitude, presence: true, format: { with: /\d{1,3}\.\d{4}/ }
+	validates :hemi_e_w, presence: { message: "E\\W Hemisphere must be specified."}
 end
