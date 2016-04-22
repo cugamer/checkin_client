2e59daa8047d882db95b1d02b8cecfef7760e82b
Jonathon <jonathon.nordquist@gmail.com>
2016-04-22 12:36:33 -0700

    Implement getAllLocationsByCompanyId helper method.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,7 @@
 module LocationsHelper
+	def getAllLocationsByCompanyId(compID)
+		baseUrl = "localhost:9393"
+		# Returns an array of hashes which contain the locations information.
+		return HTTParty.get("http://#{baseUrl}/locations/?company_id=#{compID}").parsed_response["data"]
+	end
 end
