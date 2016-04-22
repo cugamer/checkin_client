ddbc39eed8cc55aa41dc0dee118cdf4dc148bc2f
Jonathon <jonathon.nordquist@gmail.com>
2016-04-22 13:53:04 -0700

    Update error output from locations controller.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,25 +8,14 @@ class LocationsController < ApplicationController
   end
 
   def create
-  	# # client_id = session["warden.user.client.key"][0][0]
-  	# # client = Client.find(client_id)
-  	# # loc = client.locations.new(get_locations_params)
-  	# # if loc.save
-  	# # 	flash[:notice] = "Your new location has been recorded"
-  	# # 	redirect_to locations_new_path
-  	# # else
-  	# # 	@errors = loc.errors.full_messages
-  	# # 	p @errors
-   # p "================================="
     new_loc = addLocation(get_locations_params)
-    p "New loc: #{new_loc}"
     if new_loc.key?("errors")
   		flash[:alert] = "There was a problem with your new location"
       @errors = new_loc["errors"]
-      redirect_to locations_new_path
+      render locations_new_path
     else
       flash[:notice] = "Your new location has been recorded"
-      render locations_new_path
+      redirect_to locations_new_path
     end
   end
 
